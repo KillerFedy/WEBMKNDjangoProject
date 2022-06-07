edb97ecd80870b171b165964a9f5d0ebc31820cf
foggy21 <rashit.xaliullin@inbox.ru>
2022-06-07 11:18:40 +1000

    Added and customized admin profile

--- RashFedProject/shop/admin.py
+++ RashFedProject/shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Products
+from .models import Categories
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'subcategory', 'price')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'subcategory')
+    list_display_links = ('id', 'name', 'subcategory')
+    search_fields = ('id', 'name', 'subcategory')
+
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Categories, CategoriesAdmin)
--- RashFedProject/shop/models.py
+++ RashFedProject/shop/models.py
@@ -1,20 +1,31 @@
+from tabnanny import verbose
 from unicodedata import category
 from django.db import models
 
 class Products(models.Model):
-    name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
-    subcategory = models.CharField(max_length=50)
-    price = models.PositiveSmallIntegerField()
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    name = models.CharField(max_length=50, verbose_name='Продукт')
+    category = models.CharField(max_length=50, verbose_name='Категория')
+    subcategory = models.CharField(max_length=50, verbose_name='Подкатегория')
+    price = models.PositiveSmallIntegerField(verbose_name="Цена")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
 
     def __str__(self):
         return self.name
+
+    class Meta():
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ['-price']
 
 class Categories(models.Model):
-    name = models.CharField(max_length=50)
-    subcategory = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    name = models.CharField(max_length=50, verbose_name="Категория")
+    subcategory = models.CharField(max_length=50, verbose_name="Подкатегория")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
 
     def __str__(self):
         return self.name
+
+    class Meta():
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['subcategory']
